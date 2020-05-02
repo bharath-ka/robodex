d43bdb6b9e78d1d255abd6d90c0514aa08b4d583
Bharath Karanth <bharathkaranth07@gmail.com>
2020-05-02 08:47:46 +0530

    Added Cards component

---
+++ src/CardList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+    return (
+        <div>
+            {
+                robots.map(item => (<Card key={item.id} name={item.name} email={item.email} id={item.id} />))
+            }
+        </div>
+    )
+}
+
+export default CardList;
