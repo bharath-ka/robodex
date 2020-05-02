90dd8731396630334507a26eee104aea7683e8ae
Bharath Karanth <bharathkaranth07@gmail.com>
2020-05-02 08:45:14 +0530

    Cleanup for starting the project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CardList from './CardList';
+import { robots } from './robots';
+const App = () => {
+    return (
+        <div>
+            <CardList robots={robots} />
+        </div>
+    )
 }
 
 export default App;
