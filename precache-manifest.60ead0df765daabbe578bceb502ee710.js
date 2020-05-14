eda3bc90b69b34202adbfab9da8e886e0c6afbfe
Bharath Karanth <bharathkaranth07@gmail.com>
2020-05-14 17:47:17 +0530

    Updates

---
+++ precache-manifest.60ead0df765daabbe578bceb502ee710.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18788a32b3e9e831502710092082247c",
+    "url": "/robodex/index.html"
+  },
+  {
+    "revision": "4c0a7d1ab0c49fe61695",
+    "url": "/robodex/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "31814db2d6e1c515aafc",
+    "url": "/robodex/static/css/main.6ccaf1e5.chunk.css"
+  },
+  {
+    "revision": "4c0a7d1ab0c49fe61695",
+    "url": "/robodex/static/js/2.8bb135af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robodex/static/js/2.8bb135af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31814db2d6e1c515aafc",
+    "url": "/robodex/static/js/main.23dd24e2.chunk.js"
+  },
+  {
+    "revision": "ae291d6c0d22e2fd54cd",
+    "url": "/robodex/static/js/runtime-main.fb3d1593.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robodex/static/media/SEGA.12a3db8b.woff"
+  }
+]);
